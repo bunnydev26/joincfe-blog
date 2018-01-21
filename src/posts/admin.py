2214fd1782855bb154904eb715d45e376ee4d457
Dev <amithapa1994@gmail.com>
2018-01-21 18:18:33 +0530

    Upto views and templating

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -4,7 +4,9 @@ from .models import Post
 # Register your models here.
 #@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-	list_display = ('id', 'title')
-	pass
+	list_display = ['title', 'content', 'created', 'last_updated']
+	list_display_links = ['content']
+	list_filter = ['last_updated', 'created']
+	search_fields = ['title']
 
 admin.site.register(Post, PostAdmin)
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post
 # Create your views here.
+
+def post_create(request):
+	return HttpResponse("Hello this is posts Create")
+
+def post_detail(request, id=None):
+	context = {}
+	post_instance = get_object_or_404(Post, id=id)
+	context['post_instance'] = post_instance
+	return render(request, 'post_detail.html', context=context)
+
+def post_list(request):
+	context = {}
+	context['post_list'] = Post.objects.all()
+	return render(request, 'index.html', context=context)
+
+def post_update(request):
+	return HttpResponse("Hello this is posts update")
+
+def post_delete(request):
+	return HttpResponse("Hello this is posts delete")
+
